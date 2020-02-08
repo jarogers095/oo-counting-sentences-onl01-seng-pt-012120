7a4010b67309b76313c4d333088fc9d567a429b2
James Rogers <jarogers095@gmail.com>
2020-02-08 06:26:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(/!|\?|\./).delete_if do |chunk|
+      chunk.length == 0
+    end
+    
+    return sentences.size
   end
 end
